229d6fe2525dc170a57e7c6a9d8dc42b9afc820a
Aleksandra Jagiełło <jagiello013@gmail.com>
2025-11-26 14:23:36 +0100

    Update zad3.sql

---
+++ 01_setup/zad3.sql
@@ -0,0 +1,113 @@
+CREATE EXTENSION postgis;
+
+CREATE TABLE obiekty (
+    id SERIAL PRIMARY KEY,
+    name TEXT,
+    geom geometry
+);
+
+INSERT INTO obiekty(name, geom) VALUES
+('obiekt1',
+ ST_CurveToLine(
+   ST_GeomFromText(
+     'COMPOUNDCURVE(
+        (0 1, 1 1),
+        CIRCULARSTRING(1 1, 2 0, 3 1),
+        CIRCULARSTRING(3 1, 4 2, 5 1),
+        (5 1, 6 1)
+     )'
+   )
+ )
+);
+
+INSERT INTO obiekty (name, geom) VALUES 
+('obiekt2', 
+    ST_CurveToLine(
+        ST_GeomFromText(
+            'CURVEPOLYGON(
+                COMPOUNDCURVE(
+                    (10 2, 10 6),
+                    (10 6, 14 6),
+                    CIRCULARSTRING(14 6, 16 4, 14 2),
+                    CIRCULARSTRING(14 2, 12 0, 10 2)
+                ),
+                CIRCULARSTRING(11 2, 13 2, 11 2)
+            )'
+        )
+    )
+);
+
+INSERT INTO obiekty(name, geom) VALUES
+('obiekt3',
+ ST_GeomFromText(
+   'POLYGON((7 15, 10 17, 12 13, 7 15))'
+ )
+);
+
+INSERT INTO obiekty(name, geom) VALUES
+('obiekt4',
+ ST_GeomFromText(
+   'LINESTRING(
+      20 20, 25 25, 27 24, 25 22, 26 21, 22 19, 20.5 19.5
+   )'
+ )
+);
+
+INSERT INTO obiekty(name, geom)
+VALUES (
+  'obiekt5',
+  ST_Collect(
+    ST_SetSRID(ST_MakePoint(30, 30, 59), 0),
+    ST_SetSRID(ST_MakePoint(38, 32, 234), 0)
+  )
+);
+
+INSERT INTO obiekty(name, geom) VALUES
+('obiekt6',
+ ST_Collect(
+     ST_GeomFromText('LINESTRING(1 1, 3 2)'),
+     ST_Point(4, 2)
+ )
+);
+
+
+-- Zadania
+
+-- 2.
+SELECT 
+    ST_Area(
+        ST_Buffer(
+            ST_ShortestLine(
+                (SELECT geom FROM obiekty WHERE name='obiekt3'),
+                (SELECT geom FROM obiekty WHERE name='obiekt4')
+            ),
+        5)
+    ) AS pole_bufora;
+
+-- 3.
+-- Żeby LINESTRING można było zmienić na POLYGON, musi być zamknięty 
+-- (pierwszy i ostatni punkt takie same).
+UPDATE obiekty
+SET geom = ST_AddPoint(geom, ST_StartPoint(geom))
+WHERE name='obiekt4';
+
+UPDATE obiekty
+SET geom = ST_MakePolygon(geom)
+WHERE name='obiekt4';
+
+
+-- 4.
+INSERT INTO obiekty(name, geom)
+SELECT
+    'obiekt7',
+    ST_Union(
+        (SELECT geom FROM obiekty WHERE name='obiekt3'),
+        (SELECT geom FROM obiekty WHERE name='obiekt4')
+    );
+
+-- 5.
+SELECT 
+    name,
+    ST_Area(ST_Buffer(geom, 5)) AS pole_bufora
+FROM obiekty
+WHERE NOT ST_HasArc(geom);
