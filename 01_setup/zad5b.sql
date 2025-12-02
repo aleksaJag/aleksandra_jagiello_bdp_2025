9ec5afda0a4666487835a896c24155b4d1c99177
Aleksandra Jagiełło <jagiello013@gmail.com>
2025-12-02 22:56:16 +0100

    Update zad5b.sql

---
+++ 01_setup/zad5b.sql
@@ -0,0 +1,6 @@
+-- b)
+SELECT p.id_pracownika
+FROM ksiegowosc.pracownicy p
+JOIN ksiegowosc.wynagrodzenie w ON p.id_pracownika = w.id_pracownika
+JOIN ksiegowosc.pensja s ON w.id_pensji = s.id_pensji
+WHERE s.kwota > 1000;
